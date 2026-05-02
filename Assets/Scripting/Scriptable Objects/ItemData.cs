2f48b9c57a4bf005e06639be1ce2e0be11b5be0c
Hellvangelion <felis.maclellan@gmail.com>
2026-05-02 15:51:58 -0400

    modified ItemPickup, PlayerInventory, ItemData

--- Assets/Scripting/Scriptable Objects/ItemData.cs
+++ Assets/Scripting/Scriptable Objects/ItemData.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ItemData : ScriptableObject
 
 {
+    [SerializeField] private ItemData type;
     [SerializeField] private string itemName;
     [SerializeField] private Sprite icon;
     [SerializeField] private int value;
     [SerializeField] private bool isConsumable;
+
+    public ItemData Type() { return type; }
+    public string ItemName() { return itemName; }
+    public Sprite Icon() { return icon; }
+    public int Value() { return value; }
+    
 
 }
