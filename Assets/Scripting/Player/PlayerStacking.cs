59cc896e74fbdfcc0ab8d860ace3e614f4300f02
Hellvangelion <felis.maclellan@gmail.com>
2026-05-05 16:36:59 -0400

    updated PlayerStacking.cs

--- Assets/Scripting/Player/PlayerStacking.cs
+++ Assets/Scripting/Player/PlayerStacking.cs
@@ -1,16 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStacking : MonoBehaviour
 {
-    [SerializeField] private Color color;
-    void Start()
+    int stackCount = 0;
+   // SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // add to stack, color change
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            stackCount = ++stackCount;
+            Debug.Log($"Collected{stackCount}speed boosts");
+            other.GetComponent<SpriteRenderer>();
+
+        }
+        // add maximum stack capacity logic
     }
+
+  
 }
